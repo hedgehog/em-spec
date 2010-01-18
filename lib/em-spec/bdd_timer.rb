f69de81a8f31c7f36313bf50a880be3f8f7e2829
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-19 02:13:59 +1100

    em_bdd_timeout method adds EM timer

--- lib/em-spec/bdd_timer.rb
+++ lib/em-spec/bdd_timer.rb
@@ -9,6 +9,7 @@ module EventMachine
 
     def em_bdd_timeout(time_to_run)
       em_bdd_cancel_timer
+      em_bdd_timer=::EM.add_timer(time_to_run) { done; raise BddTimeoutExceededError.new }
     end
 
   end
