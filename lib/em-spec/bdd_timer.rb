aafd0e2b7499296b3feac932646a9f8fec56c9bf
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-19 02:16:34 +1100

    ensure EM namespace is correct.

--- lib/em-spec/bdd_timer.rb
+++ lib/em-spec/bdd_timer.rb
@@ -5,7 +5,7 @@ module EventMachine
     attr_accessor :em_bdd_timer
 
     def em_bdd_cancel_timer
-      EM.cancel_timer(em_bdd_timer) if em_bdd_timer
+      ::EM.cancel_timer(@em_bdd_timer) if @em_bdd_timer
     end
 
     def em_bdd_timeout(time_to_run)
