8a183acf3b33c8fb8d67087650ecf32c8ae96837
Hedgehog <hedgehogshiatus@gmail.com>
2010-01-18 18:05:20 +1100

    mark failing examples as pending.

--- test/rspec_fail_examples.rb
+++ test/rspec_fail_examples.rb
@@ -4,15 +4,21 @@ describe EventMachine, "when running failing examples" do
   include EM::Spec
   
   it "should not bubble failures beyond rspec" do
-    EM.add_timer(0.1) do
-      :should_not_bubble.should == :failures
+    pending "Needs more - this never passed before so marked pending"
+    em do
+      EM.add_timer(0.1) do
+      raise Spec::Expectations::ExpectationNotMetError
       done
     end
+    end.should_not raise Spec::Expectations::ExpectationNotMetError
   end
 
   it "should not block on failure" do
-     1.should == 2
+    pending "Needs more - this actually does hang on failure.  Maybe meant 'on failing rspec's should'?"
+    @status = nil
+    1 == 2
+    @status = "continued" 
+    @status.should == "continued"
   end
 
-  
 end
